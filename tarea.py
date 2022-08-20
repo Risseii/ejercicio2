b6c5b4e59d6369a01b8587014a8b01f4c568cef7
Vania Belén <vany@MacBook-Pro-de-Vania.local>
2022-08-20 18:12:36 -0500

    commit

--- tarea.py
+++ tarea.py
@@ -13,8 +13,6 @@ df_expected.sample(2).T
 st.write('Shape df_expected')
 df_expected.shape
 
-st.write('Number of unique values : Retail pdt sku')
-df_expected["Retail_Product_SKU"].nunique()
 
 st.write('Sample df_counted')
 df_counted.sample(2).T
@@ -23,8 +21,8 @@ st.write('Shape df_counted')
 df_counted.shape
 
 #removing duplicates RFID
-st.write('Removing RFID duplicates')
 df_counted = df_counted.drop_duplicates("RFID")
+df_counted.sample(2).T
 
 st.write('Group by')
 df_B = df_counted.groupby("Retail_Product_SKU").count()[["RFID"]].reset_index().rename(columns={"RFID":"Retail_CCQTY"})
@@ -46,11 +44,6 @@ st.write('Head')
 df_A = df_expected[my_cols_selected]
 df_A.head().T
 
-st.write('Discrepancy head')
-df_discrepancy = pd.merge(df_A, df_B, how="outer", left_on="Retail_Product_SKU", right_on="Retail_Product_SKU")
-df_discrepancy.head()
-df_discrepancy['Retail_CCQTY'] = df_discrepancy['Retail_CCQTY'].fillna(0)
-df_discrepancy["Retail_CCQTY"] = df_discrepancy["Retail_CCQTY"].astype(int)
-df_discrepancy.head()
+
 
 
